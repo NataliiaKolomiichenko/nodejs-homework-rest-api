6eb2ace9f22095d6cfb2d25ecc8ec48a3fd2fffa
NataliiaKolomiichenko <gella1996nr@gmail.com>
2023-01-08 01:31:20 +0200

    hw-05 done

--- controller/auth/register.js
+++ controller/auth/register.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
-const User = require('../../models/user');
+const gravatar = require('gravatar');
+const { User } = require('../../models');
 const { HttpError } = require('../../helpers');
 
 const register = async (req, res) => {
@@ -9,7 +10,9 @@ const register = async (req, res) => {
         throw HttpError(409, 'Email in use');
     }
     const hashPassword = await bcrypt.hash(password, 10);
-    const newUser = await User.create({ ...req.body, password: hashPassword });
+    const avatarURL = gravatar.url(email);
+
+    const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL });
 
     res.status(201).json({
         email: newUser.email,
